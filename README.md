6c7fe12ae4149f9e29ca590075a577ee6ad5df25
Kazumoto <kazumoto.kondo@gmail.com>
2023-09-24 15:24:51 +0900

    1st

---
+++ README.md
@@ -0,0 +1,17 @@
+## Overview
+
+This is a tool for entering a password, which connects to your computer using Bluetooth.
+To enter the preset password, press the BOOTSEL button.
+
+## Hardware
+
+- Raspberry pi pico W
+- USB battery or external battery
+
+## Peripheral
+
+- USB cable to write firmware
+
+## WARNING!
+
+- When connecting the PC and Raspberry Pi pico W with a USB cable, the external power supply must be disconnected.
---
+++ RPI_pico_W_keyboard/ble_pw.ino
@@ -0,0 +1,31 @@
+#include <KeyboardBT.h>
+
+void ledCB(bool numlock, bool capslock, bool scrolllock, bool compose, bool kana, void *cbData) {
+  (void) numlock;
+  (void) scrolllock;
+  (void) compose;
+  (void) kana;
+  (void) cbData;
+  digitalWrite(LED_BUILTIN, capslock ? HIGH : LOW);
+}
+
+void setup() {
+  KeyboardBT.begin("pcw_mcr"); // this name will be displayed on your PC
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  KeyboardBT.onLED(ledCB);
+  KeyboardBT.begin();
+  delay(5000);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+  if (BOOTSEL) {
+    KeyboardBT.print("ASDFGKndK123!@#");
+    while (BOOTSEL);
+  }
+}
